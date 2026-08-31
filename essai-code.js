a5874f754929ca64080e7566b083adecdb36e977
Blondel76 <155725516+Blondel76@users.noreply.github.com>
2026-08-31 13:04:09 +0200

    Add custom card definition for Home Assistant

--- essai-code.js
+++ essai-code.js
@@ -29,3 +29,11 @@ class EssaiDeCodeCard extends HTMLElement {
 }
 
 customElements.define("essai-de-code", EssaiDeCodeCard);
+window.customCards = window.customCards || [];
+
+window.customCards.push({
+  type: "essai-de-code",
+  name: "Essai de code",
+  description: "Ma première carte Home Assistant",
+  preview: true,
+});
